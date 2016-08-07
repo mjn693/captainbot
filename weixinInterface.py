e3e436a044d887ac8ec91bab11a6b0eeb0c09875
Miaotianzi Jin <jinmiaotianzi@gmail.com>
2016-08-06 23:51:57 -0500

    translator

--- weixinInterface.py
+++ weixinInterface.py
@@ -37,22 +37,35 @@ class WeixinInterface:
 
     def youdao(word):
         qword = urllib2.quote(word)
-        baseurl = r'http://fanyi.youdao.com/openapi.do?keyfrom=yourAppName&key=yourAppKey&type=data&doctype=json&version=1.1&q='
+        baseurl =r'http://fanyi.youdao.com/openapi.do?keyfrom=<keyfrom>&key=<key>&type=data&doctype=json&version=1.1&q='
         url = baseurl+qword
         resp = urllib2.urlopen(url)
         fanyi = json.loads(resp.read())
-        ##根据json是否返回一个叫“basic”的key来判断是否翻译成功
-        if 'basic' in fanyi.keys():
-        ##下面是你自已来组织格式
-            trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'],''.join(fanyi['translation']),''.join(fanyi['basic']['explains']),''.join(fanyi['web'][0]['value']))
-            return trans
+        if fanyi['errorCode'] == 0:        
+            if 'basic' in fanyi.keys():
+                trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'],''.join(fanyi['translation']),' '.join(fanyi['basic']['explains']),''.join(fanyi['web'][0]['value']))
+                return trans
+            else:
+                trans =u'%s:\n基本翻译:%s\n'%(fanyi['query'],''.join(fanyi['translation']))        
+                return trans
+        elif fanyi['errorCode'] == 20:
+            return u'对不起，要翻译的文本过长'
+        elif fanyi['errorCode'] == 30:
+            return u'对不起，无法进行有效的翻译'
+        elif fanyi['errorCode'] == 40:
+            return u'对不起，不支持的语言类型'
         else:
-            return u'对不起，您输入的单词%s无法翻译，请检查拼写'% word        
+            return u'对不起，您输入的单词%s无法翻译,请检查拼写'% word   
             
     def POST(self):        
         str_xml = web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行XML解析
-        content=xml.find("Content").text#获得用户所输入的内容
+        content=xml.find("Content").text#获得用户所输入的内容 
+        if type(content).__name__ == "unicode":
+            content = content.encode('UTF-8')            
+        Nword = youdao(content)        
+        return self.render.reply_text(fromUser,toUser,int(time.time()),Nword)
+        
         msgType=xml.find("MsgType").text
         fromUser=xml.find("FromUserName").text
         toUser=xml.find("ToUserName").text        
